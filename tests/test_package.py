9426d68e85284cea483750ab3af26cc5179506e9
Yacine Haddad <yhaddad@cern.ch>
2021-01-27 14:03:10 +0100

    changing setup.py

--- tests/test_package.py
+++ tests/test_package.py
@@ -3,3 +3,9 @@ import lhctodd as m
 
 def test_version():
     assert m.__version__
+
+def test_data():
+    try:
+        m.list()
+    except:
+        raise RuntimeError('Failed to fetch data from database')
